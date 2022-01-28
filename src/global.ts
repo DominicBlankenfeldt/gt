b023db4114b6c94b78f5622042553f638b55cbbf
tim <t.littschwager@mbd-team.de>
2022-01-28 09:31:41 +0100

    refactor

--- src/global.ts
+++ src/global.ts
@@ -37,64 +37,37 @@ export const player = ref({
 });
 //finish me
 export function checkPlayer(checkedPlayer: type.Player) {
-  if (checkedPlayer.id === undefined) {
-    checkedPlayer.id = ""
-  }
-  if (checkedPlayer.vector === undefined) {
-    checkedPlayer.vector = [0, 0]
-  }
-  if (checkedPlayer.speed === undefined) {
-    checkedPlayer.speed = 5
-  }
-  if (checkedPlayer.skillTree === undefined) {
-    checkedPlayer.skillTree = {
-      skillPoints: 0,
-      usedSkillPoints: 0,
-      skills: [
-        { name: "fastAbility", lvl: 0, maxlvl: 1 },
-        { name: "slowAbility", lvl: 0, maxlvl: 1 },
-        { name: "bombAbility", lvl: 0, maxlvl: 1 },
-        { name: "slowEnemy", lvl: 0, maxlvl: 20 },
-        { name: "spawnLessEnemy", lvl: 0, maxlvl: 20 },
-        { name: "scoreMultiplicator", lvl: 0, maxlvl: 20 },
-        { name: "betterCoin", lvl: 0, maxlvl: 20 },
-        { name: "longerMagnet", lvl: 0, maxlvl: 20 },
-        { name: "betterGrowPotion", lvl: 0, maxlvl: 20 },
-        { name: "smallerBlackHole", lvl: 0, maxlvl: 20 },
+  checkedPlayer=checkedPlayer||{}
+  checkedPlayer.id=checkedPlayer.id||""
+  checkedPlayer.vector=checkedPlayer.vector||[0, 0]
+  checkedPlayer.speed=checkedPlayer.speed||5
+  checkedPlayer.skillTree=checkedPlayer.skillTree||{
+    skillPoints: 0,
+    usedSkillPoints: 0,
+    skills: [
+      { name: "fastAbility", lvl: 0, maxlvl: 1 },
+      { name: "slowAbility", lvl: 0, maxlvl: 1 },
+      { name: "bombAbility", lvl: 0, maxlvl: 1 },
+      { name: "slowEnemy", lvl: 0, maxlvl: 20 },
+      { name: "spawnLessEnemy", lvl: 0, maxlvl: 20 },
+      { name: "scoreMultiplicator", lvl: 0, maxlvl: 20 },
+      { name: "betterCoin", lvl: 0, maxlvl: 20 },
+      { name: "longerMagnet", lvl: 0, maxlvl: 20 },
+      { name: "betterGrowPotion", lvl: 0, maxlvl: 20 },
+      { name: "smallerBlackHole", lvl: 0, maxlvl: 20 },
 
-      ] as type.Skill[],
-    } as type.SkillTree
-  }
-  if (checkedPlayer.outlook === undefined) {
-    checkedPlayer.outlook = "up"
-  }
-  if (checkedPlayer.highscore === undefined) {
-    checkedPlayer.highscore = 0
-  }
-  if (checkedPlayer.highscoreHardcore) {
-    checkedPlayer.highscoreHardcore = 0
-  }
-  if (checkedPlayer.email === undefined) {
-    checkedPlayer.email = "gast@"
-  }
-  if (checkedPlayer.username === undefined) {
-    checkedPlayer.username = "gast"
-  }
-  if (checkedPlayer.img === undefined) {
-    checkedPlayer.img = ""
-  }
-  if (checkedPlayer.registeredAt === undefined) {
-    checkedPlayer.registeredAt = 0
-  }
-  if (checkedPlayer.playedGames === undefined) {
-    checkedPlayer.playedGames = 0
-  }
-  if (checkedPlayer.playedHardcore === undefined) {
-    checkedPlayer.playedHardcore = 0
-  }
-  if (checkedPlayer.hardcoreMode === undefined) {
-    checkedPlayer.hardcoreMode = false
-  }
+    ] as type.Skill[],
+  } as type.SkillTree
+  checkedPlayer.outlook=checkedPlayer.outlook||"up"
+  checkedPlayer.highscore=checkedPlayer.highscore ||0 
+  checkedPlayer.highscoreHardcore=checkedPlayer.highscoreHardcore||0
+  checkedPlayer.email=checkedPlayer.email||"gast@"
+  checkedPlayer.username=checkedPlayer.username||"gast"
+  checkedPlayer.img=checkedPlayer.img||""
+  checkedPlayer.registeredAt=checkedPlayer.registeredAt||0
+  checkedPlayer.playedGames=checkedPlayer.playedGames||0
+  checkedPlayer.playedHardcore=checkedPlayer.playedHardcore||0
+  checkedPlayer.hardcoreMode=checkedPlayer.hardcoreMode||false
   for (const skill of ["fastAbility", "slowAbility", "bombAbility"]) {
     if (checkSkill(checkedPlayer, skill)) {
       checkedPlayer.skillTree.skills.push({ name: skill as type.Skillname, lvl: 0, maxlvl: 1 })
