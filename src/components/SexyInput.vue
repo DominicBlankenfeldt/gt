fd9d4fdb9cf83a8cec8748aedd7cfd846171c595
tim <t.littschwager@mbd-team.de>
2022-05-09 16:07:34 +0200

    select to datalist

--- src/components/SexyInput.vue
+++ src/components/SexyInput.vue
@@ -4,33 +4,52 @@
             v-if="type != 'textarea'"
             v-bind="$attrs"
             class="form-control shadow-none"
-            :id="id"
-            :type="type"
+            :type="viewPassword ? 'text' : type"
             :value="modelValue"
             @input="updateValue"
             :class="{ dirty: modelValue }"
-            name=""
-            :list="id2"
+            :list="id"
             autocomplete="off"
         />
-
         <textarea
             v-if="type == 'textarea'"
             v-bind="$attrs"
-            :id="id"
             class="form-control shadow-none"
             @input="updateValue"
             :value="modelValue"
             :class="{ dirty: modelValue }"
             rows="3"
         ></textarea>
-        <label class="placeholder-text" :for="id">
+        <label class="placeholder-text">
             <div class="text">{{ placeholder }}</div>
         </label>
-        <datalist :id="id2" v-if="options">
+        <datalist :id="id" v-if="options">
             <option :value="optionKey ? option[optionKey] : option" v-for="option of options" :key="JSON.stringify(option)"></option>
         </datalist>
         <button v-if="btnText" type="button" @click="affirm()" :class="btnClass">{{ btnText }}</button>
+        <button v-if="type == 'password'" type="button" @click="showPassword()" :class="btnClass">
+            <template v-if="!viewPassword">
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                    <path
+                        d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"
+                    />
+                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                </svg>
+            </template>
+            <template v-else>
+                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-eye-slash" viewBox="0 0 16 16">
+                    <path
+                        d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"
+                    />
+                    <path
+                        d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"
+                    />
+                    <path
+                        d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"
+                    />
+                </svg>
+            </template>
+        </button>
     </div>
 </template>
 
@@ -43,12 +62,11 @@ export default defineComponent({
     data() {
         return {
             id: '',
-            id2: '',
+            viewPassword: false,
         }
     },
     mounted() {
-        this.id = JSON.stringify(Math.random())
-        this.id2 = JSON.stringify(Math.random())
+        this.id = 'input' + Math.random()
         if (this.type == 'date') {
             if (this.modelValue.length == 10) {
                 return
@@ -87,6 +105,9 @@ export default defineComponent({
         },
     },
     methods: {
+        showPassword() {
+            this.viewPassword = !this.viewPassword
+        },
         updateValue(event: Event | string) {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
@@ -147,6 +168,7 @@ export default defineComponent({
             display: block !important;
         }
         &::-webkit-calendar-picker-indicator {
+            display: block;
             cursor: pointer;
             height: auto;
             width: 5%;
